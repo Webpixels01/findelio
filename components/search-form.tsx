0a902ce054ada4554d3d08c11a1f4626bc889b22
Jan Cabanik <jan.cabanik@gmail.com>
2026-08-10 19:44:42 +0200

    style: increase search button height

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -110,7 +110,7 @@ export default async function SearchForm({
         />
       </label>
 
-      <button type="submit" className="primary-button mt-auto h-12 px-5">
+      <button type="submit" className="primary-button mt-auto h-15 px-5">
         {t("submit")}
       </button>
     </form>
